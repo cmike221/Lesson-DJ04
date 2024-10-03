fd0cfb39b7592db65dab21c24d12eb9d2e1f4a44
cmike221 <cmike221@yandex.ru>
2024-10-03 10:36:32 +0300

    Добавлен стильный дизайн

--- movie_project/films/forms.py
+++ movie_project/films/forms.py
@@ -6,3 +6,8 @@ class FilmForm(forms.ModelForm):
     class Meta:
         model = Film
         fields = ['title', 'description', 'review']
+
+    def __init__(self, *args, **kwargs):
+        super(FilmForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
--- movie_project/movie_project/urls.py
+++ movie_project/movie_project/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('films.urls')),
     path('films/', include('films.urls')),
 ]
